a4a82178bfadaf1ddf5620bddd8e23fe97c6b8c5
Oliver Cvetkovski <oliver.cvetkovski@thethingsindustries.com>
2023-01-17 02:38:55 +0100

    Add k8s util scripts

---
+++ k8s/createall.sh
@@ -0,0 +1,14 @@
+helm install redis --set auth.password=$REDIS_PASSWORD --set replica.replicaCount=3 bitnami/redis
+
+# create secrets
+kubectl create secret generic auth-secret --from-file=./secrets/gcp/auth.gcp.config.yaml
+kubectl create secret generic keys-secret --from-file=./secrets/gcp/keys.gcp.config.yaml
+kubectl create secret generic qrcode-secret --from-file=./secrets/gcp/qrcode.gcp.config.yaml
+kubectl create secret generic slots-secret --from-file=./secrets/gcp/slots.gcp.config.yaml
+kubectl create secret generic sa-secret --from-file=./secrets/zvax-project-access-key.json
+
+# run deployments
+kubectl apply -f ./auth.deployment.yaml
+kubectl apply -f ./keys.deployment.yaml
+kubectl apply -f ./qrcode.deployment.yaml
+kubectl apply -f ./slots.deployment.yaml
--- k8s/deleteall.sh
+++ k8s/deleteall.sh
@@ -1,4 +1,4 @@
 kubectl delete deployment auth-deployment keys-deployment qrcode-deployment slots-deployment
 kubectl delete service auth-service keys-service qrcode-service slots-service
-kubectl delete secret auth-secret keys-secret qrcode-secret slots-secret
+kubectl delete secret auth-secret keys-secret qrcode-secret slots-secret sa-secret
 helm uninstall redis
